491aab13a8501e1d921b0ad03e45c331752f420b
Zala145 <jakub.zalecki@gmail.com>
2024-11-04 14:56:18 +0100

    Update calculatePoints.js

--- src/utils/calculatePoints.js
+++ src/utils/calculatePoints.js
@@ -1,7 +1,7 @@
 const calculatePoints = (amount) => {
   let points = 0;
 
-  if (amount < 50 || amount === 50) { return 0 };
+  if (amount <= 50) { return 0 };
   if (amount > 50 && amount <= 100) { points += (amount - 50); }
   if (amount > 100) { points += ((amount - 100) * 2) + 50;  }
 
